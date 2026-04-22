0d24c0b5ae5ebab69fcab64a56df8b8f73672c35
Riya Shrestha <shrer488@newschool.edu>
2026-04-22 02:27:44 -0400

    added links and copy changes

--- scripts/popup.js
+++
@@ -1,8 +0,0 @@
-let seeBowlButton = document.getElementById("seeBowl");
-
-let tabUrl = new URL(window.location.href);
-console.log(tabUrl)
-
-if (tabUrl.host == "chatgpt.com"){
-    seeBowlButton.style.display="block";
-}
